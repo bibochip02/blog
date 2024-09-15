eadaf36749bfac1993bfc34de3357d073a99987a
hung.bui <bibochip02@gmail.com>
2024-09-15 15:22:45 +0700

    add codecov token

--- spec/models/brand_spec.rb
+++ spec/models/brand_spec.rb
@@ -2,6 +2,6 @@ require 'rails_helper'
 
 RSpec.describe Brand, type: :model do
   describe 'associations' do
-    # it { should belong_to(:country) }
+    it { should belong_to(:country) }
   end
 end
