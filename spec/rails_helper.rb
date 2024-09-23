9d737858ef4472bd9b2a2fb0fb22e69071b63070
buihung-wogi <bui.hung@wogi.biz>
2024-09-23 22:42:34 +0700

    update github action

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -14,8 +14,14 @@ end
 
 ActiveRecord::Migration.maintain_test_schema!
 
-SimpleCov::Formatter::LcovFormatter.config.report_with_single_file = true
-
-SimpleCov.formatter = SimpleCov::Formatter::LcovFormatter
+SimpleCov.formatter = SimpleCov::Formatter::CoberturaFormatter
 
 SimpleCov.start
+Coveralls.wear!('rails')
+
+begin
+  ActiveRecord::Migration.maintain_test_schema!
+rescue ActiveRecord::PendingMigrationError => e
+  puts e.to_s.strip
+  exit 1
+end
